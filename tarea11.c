124787c22aaf1e07f5b89e4b01e9ad4fc5b9470e
borja-urrea <borjaurrea9@gmail.com>
2026-04-06 01:28:58 +0000

    .

--- tarea11.c
+++ tarea11.c
@@ -4,6 +4,18 @@
 #include "tdas/list.h"
 #include "tdas/extra.h"
 #include "tdas/queue.h"
+#include <ctype.h>
+
+typedef struct {
+  char descripcion[50];
+  char ncategoria[50];
+} tarea;
+
+void aMayusculas(char *cadenaAmayus){
+  for (int i = 0; cadenaAmayus[i]; i ++){
+    cadenaAmayus[i] = toupper((unsigned char)cadenaAmayus[i]);
+  }
+}
 
 void mostrarOpciones(){
   printf("=========================\n");
@@ -12,15 +24,28 @@ void mostrarOpciones(){
   printf("1. Agregar nueva categoria\n");
   printf("2. Eliminar una categoria\n");
   printf("3. Mostrar categorias almacenadas\n");
+  printf("4. Ingresar tarea\n");
+  printf("5. Mostrar cola tareas\n");
   printf("0. Terminar\n");
 }
 
 void mostrarLista(List *listaCategorias){
+  printf("----- LISTA DE CATEGORIAS -----\n");
   if (listaCategorias == NULL) return;
   char *item = list_first(listaCategorias);
   while (item != NULL) {
     printf("%s\n", item);
     item = list_next(listaCategorias);
+  }
+}
+
+void mostrarCola(Queue *colaTareas){
+  printf("----- LISTA DE PENDIENTES Y SU CATEGORIA -----\n");
+  if (colaTareas == NULL) return;
+  tarea *tareaMostrar = (tarea*) queue_front(colaTareas);
+  while (tareaMostrar != NULL){
+    printf("Tarea: %20s || Categoria: %s \n", tareaMostrar -> descripcion, tareaMostrar -> ncategoria);
+    tareaMostrar = (tarea*) queue_next(colaTareas);
   }
 }
 
@@ -35,7 +60,29 @@ void eliminarCategoria(char *nombre, List *listaCategorias){
   }
 }
 
-void recolectarDatos(List *listaCategorias){
+void eliminarTareasCategoria (char *categoria, Queue *colaTareas){
+  if (colaTareas == NULL) return;
+  Queue *aux = queue_create(NULL);
+  printf("Tareas de la categoria %s eliminadas\n", categoria);
+  tarea *tareaEliminar = (tarea*) queue_front(colaTareas);
+  while(tareaEliminar != NULL){
+    if (strcmp(tareaEliminar -> ncategoria, categoria) != 0){
+      queue_insert(aux, tareaEliminar);
+    }else{
+      free(tareaEliminar);
+    }
+    tareaEliminar = (tarea*) queue_next(colaTareas);
+  }
+  queue_clean(colaTareas);
+  tarea *auxTareas = (tarea*) queue_front(aux);
+  while(auxTareas != NULL){
+    queue_insert(colaTareas, auxTareas);
+    auxTareas = (tarea*) queue_next(aux);
+  }
+  free(aux);
+}
+
+void recolectarDatos(List *listaCategorias, Queue *colaTareas){
   int dato;
   do {
     mostrarOpciones();
@@ -43,9 +90,11 @@ void recolectarDatos(List *listaCategorias){
     getchar();
     switch(dato){
       case 1: {
+        printf("Ingrese el nombre de su categoria: \n");
         char nombre[50];
         if(fgets(nombre, sizeof(nombre), stdin)){
           nombre[strcspn(nombre, "\n")] = 0;
+          aMayusculas(nombre);
           char *nombreAux = (char*)malloc((strlen(nombre)+1) * sizeof(char));
           if(nombreAux != NULL){
             strcpy(nombreAux, nombre);
@@ -56,19 +105,44 @@ void recolectarDatos(List *listaCategorias){
       }
       case 2: {
         //e.c
-        printf("Escriba la categoria que desea eliminar\n");
+        printf("Escriba la categoria que desea eliminar: \n");
         char nombre[50];
         if(fgets(nombre, sizeof(nombre), stdin)){
+          aMayusculas(nombre);
           nombre[strcspn(nombre, "\n")] = 0;
           eliminarCategoria(nombre, listaCategorias);
         }
-        //eliminar tareas
-        
+        if(queue_front(colaTareas) != NULL){
+          eliminarTareasCategoria(nombre, colaTareas);
+        }
+        break;
       }
       case 3: {
         mostrarLista(listaCategorias);
         break;
       }
+
+      case 4: {
+        tarea *tareaNueva = (tarea*) malloc(sizeof(tarea));
+        
+        printf("Ingrese categoria a la que pertenece su tarea\n");
+        fgets(tareaNueva -> ncategoria, 50, stdin);
+        tareaNueva -> ncategoria[strcspn(tareaNueva -> ncategoria, "\n")] = 0;
+        aMayusculas(tareaNueva -> ncategoria);
+        
+        printf("Ingrese el nombre de su tarea\n");
+        if (fgets(tareaNueva -> descripcion, 50, stdin)){
+          tareaNueva -> descripcion[strcspn(tareaNueva -> descripcion, "\n")] = 0;
+          queue_insert(colaTareas, tareaNueva);
+        }
+        break;
+      }
+
+      case 5: {
+        mostrarCola(colaTareas);
+        break;
+      }
+      
       case 0: {
         return;
       }
@@ -83,8 +157,8 @@ void recolectarDatos(List *listaCategorias){
     
 int main(){
   List *listaCategorias = list_create();
-  //Quehue *colaTareas = list_create(NULL);
-  recolectarDatos(listaCategorias);
+  Queue *colaTareas = queue_create(NULL);
+  recolectarDatos(listaCategorias, colaTareas);
   
   return 0;
 }
